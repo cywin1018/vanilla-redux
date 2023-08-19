b8f98bf56eead2d8f360dc3d978dd12fdfb85100
cywin1018 <cywin0821@gmail.com>
2023-08-19 15:46:26 +0900

    finish todo use v6

--- vanilla-redux/src/routes/Details.js
+++ vanilla-redux/src/routes/Details.js
@@ -1,5 +1,33 @@
+import {useSelector} from "react-redux";
+import {Link,useParams,useNavigate} from "react-router-dom";
+
+
 const Details = () => {
-    return "Details";
+    const navigate = useNavigate();
+    const param = useParams();
+    const currentState = useSelector((state)=>state);
+
+    const onClickBackBtn = () => navigate(-1);
+    const onClickHomeBtn = () => navigate("/");
+    const todo = currentState.find((item)=>item.id === parseInt(param.id));
+    const others = currentState.filter((todo)=>todo.id !== parseInt(param.id));
+
+    return (
+        <>
+            <h1>Detail of {todo.text}</h1>
+
+            <h2>Others</h2>
+            <ul>
+                {others.map((todo) => (
+                    <Link key={todo.id} to={`/${todo.id}`}>
+                        <li>{todo.text}</li>
+                    </Link>
+                ))}
+            </ul>
+            <button onClick={onClickBackBtn}> Go Back</button>
+            <button onClick={onClickHomeBtn}> Go Home</button>
+        </>
+    );
 }
 
 export default Details
--- vanilla-redux/src/routes/Home.js
+++ vanilla-redux/src/routes/Home.js
@@ -1,21 +1,48 @@
-import {useState} from "react";
+import React, { useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {addToDo,deleteToDo} from "../store";
+import {Link} from "react-router-dom";
 
 const Home = () => {
-    const [text,setText] = useState("");
-    function onChange(e){
+    const [text, setText] = useState('');
+    const todo = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const onClick = () =>{
+        const id = Date.now();
+        dispatch(deleteToDo({text,id}));
+    }
+    const onChange = (e) => {
         setText(e.target.value);
     }
-    function onSubmit(e){
-        e.preventDefault()
-        console.log(text)
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch(addToDo(text));
+        setText("");
     }
-    return (<>
-        <h1>ToDo</h1>
-        <form onSubmit={onSubmit}>
-            <input type={"text"} value={text} onChange={onChange}/>
-            <button>Add</button>
-        </form>
-        <ul></ul>
-    </>);
-}
- export default Home;
+    const btnOnClick = (e) =>{
+        const targetId = parseInt(e.target.parentNode.id);
+        dispatch(deleteToDo(targetId));
+    }
+    return (
+
+      <>
+          <h1>To Do</h1>
+          <form onSubmit={onSubmit}>
+                <input type={"text"} value={text} onChange={onChange}/>
+                <button onClick={onClick}>Add</button>
+          </form>
+          <ul>
+              {todo.map((state)=>(
+                  <li key={state.id} id={state.id}>
+                     <Link to={`${state.id}`}> {state.text}</Link>
+                      <button onClick={btnOnClick}>X</button>
+                  </li>
+              ))}
+          </ul>
+
+      </>
+
+    );
+};
+
+export default Home;
